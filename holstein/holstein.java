fa532cc1d9e2f2649b454e4a75b8707cc7815b5a
Juan Gutierrez <juanny.gee@gmail.com>
2011-07-24 10:40:43 -0400

    added Healthy Holsteins problem to repos

---
+++ holstein/holstein.java
@@ -0,0 +1,20 @@
+/*
+ID: jmg20482
+LANG: JAVA
+TASK: holstein
+*/
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class holstein {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader in = new BufferedReader(new FileReader("holstein.in"));
+    	PrintWriter out = new PrintWriter(new FileWriter("holstein.out"));
+        
+        in.close();
+        out.close();
+        System.exit(0);
+    }
+}
