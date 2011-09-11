46d87c4f25782c9c69c6d67f63974a36f5b6f8a0
Juan Gutierrez <juanny.gee@gmail.com>
2011-09-11 12:26:49 -0400

    added next exercise, longest prefix

---
+++ prefix/prefix.java
@@ -0,0 +1,23 @@
+/*
+ID: jmg20482
+LANG: JAVA
+TASK: prefix
+*/
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class prefix
+{
+    public prefix() {}
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader("prefix.in"));
+		PrintWriter out = new PrintWriter(new FileWriter("prefix.out"));
+		
+		in.close();
+		out.close();
+		System.exit(0);
+    }
+}
