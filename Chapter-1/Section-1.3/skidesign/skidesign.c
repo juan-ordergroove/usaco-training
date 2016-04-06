e1067ed346f733d2a481e1502deea29c4ebd81d9
juanny_G <juanny.gee@gmail.com>
2016-04-06 08:07:11 -0400

    fix reading input and sort hills by elevation

--- Chapter-1/Section-1.3/skidesign/skidesign.c
+++ Chapter-1/Section-1.3/skidesign/skidesign.c
@@ -6,20 +6,36 @@ TASK: skidesign
 #include <stdio.h>
 #include <stdlib.h>
 
+int N;
+int *hills;
+
+void print_hills() {
+    int i;
+    for (i=0; i < N; ++i) {
+        printf("%d\n", hills[i]);
+    }
+}
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
 int main() {
-    int i, n;
-    int *hills;
+    int i;
     FILE *infile;
     FILE *outfile;
 
     infile = fopen("skidesign.in", "r");
-    fscanf(infile, "%d", &n);
-    fclose(infile);
+    fscanf(infile, "%d", &N);
 
-    hills = malloc(n * sizeof(int));
-    for (i=0; i < n; ++i) {
+    hills = malloc(N * sizeof(int));
+    for (i=0; i < N; ++i) {
         fscanf(infile, "%d", &hills[i]);
     }
+    fclose(infile);
+
+    qsort(hills, N, sizeof(int), cmpfunc);
+    print_hills();
 
     outfile = fopen("skidesign.out", "w");
     fclose(outfile);
