e71c4b86a258a801f992b18f6c8a7920cce0d4e6
juanny_G <juanny.gee@gmail.com>
2016-04-16 15:02:13 -0400

    Implement solution for skidesign
    Brute force the solution by incrementing the elevation range until
    the maximum elevation of the set is reached and compute the cost
    of modifying all hills in that range. Track the minimum cost while
    iterating through each range.

--- Chapter-1/Section-1.3/skidesign/skidesign.c
+++ Chapter-1/Section-1.3/skidesign/skidesign.c
@@ -8,20 +8,15 @@ TASK: skidesign
 
 int N;
 int *hills;
+int HIGHEST_ELEVATION = 0;
+const int MAX_ELEVATION_DIFFERENCE = 17;
 
-void print_hills() {
-    int i;
-    for (i=0; i < N; ++i) {
-        printf("%d\n", hills[i]);
-    }
-}
-
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
-}
+int find_minimum_cost_elevation_change();
+int cost_to_modify_hill(int hill_elevation, int shortest, int tallest);
 
 int main() {
     int i;
+    int min_cost = 0;
     FILE *infile;
     FILE *outfile;
 
@@ -31,15 +26,55 @@ int main() {
     hills = malloc(N * sizeof(int));
     for (i=0; i < N; ++i) {
         fscanf(infile, "%d", &hills[i]);
+        if (hills[i] > HIGHEST_ELEVATION) {
+            HIGHEST_ELEVATION = hills[i];
+        }
     }
     fclose(infile);
 
-    qsort(hills, N, sizeof(int), cmpfunc);
-    print_hills();
+    min_cost = find_minimum_cost_elevation_change();
 
     outfile = fopen("skidesign.out", "w");
+    fprintf(outfile, "%d\n", min_cost);
     fclose(outfile);
 
     free(hills);
     return 0;
+}
+
+int find_minimum_cost_elevation_change() {
+    int i;
+    int cost;
+    int hill_elevation;
+    int shortest = 0;
+    int tallest = shortest + MAX_ELEVATION_DIFFERENCE;
+    int min_cost = 1000000;
+
+    while (tallest <= HIGHEST_ELEVATION) {
+        cost = 0;
+        for(i=0; i < N; ++i) {
+            hill_elevation = hills[i];
+            cost += cost_to_modify_hill(hill_elevation, shortest, tallest);
+        }
+
+        if (cost < min_cost) {
+            min_cost = cost;
+        }
+        ++shortest;
+        ++tallest;
+    }
+    return min_cost;
+}
+
+int cost_to_modify_hill(int hill_elevation, int shortest, int tallest) {
+    int cost;
+    if (hill_elevation >= shortest && hill_elevation <= tallest) {
+        cost = 0;
+    } else if (hill_elevation < shortest) {
+        cost = shortest - hill_elevation;
+    } else {
+        cost = hill_elevation - tallest;
+    }
+
+    return cost * cost;
 }
