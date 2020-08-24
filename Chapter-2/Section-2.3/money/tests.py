0482f4f2c35cedc78f5b29a3b8111072456e067b
Juan Gutierrez <juanny.gee@gmail.com>
2020-08-23 22:01:16 -0400

    Fix typo in test name

--- Chapter-2/Section-2.3/money/tests.py
+++ Chapter-2/Section-2.3/money/tests.py
@@ -4,7 +4,7 @@ from .money import MoneySystem
 
 
 class MoneySystemsTestCase(unittest.TestCase):
-    def test_there_are_ways_to_construct_a_value_when_all_coins_are_larger_than_the_target_value(self):
+    def test_there_are_no_ways_to_construct_a_value_when_all_coins_are_larger_than_the_target_value(self):
         coins = [10, 20, 30]
         target_value = 1
         money_sys = MoneySystem(target_value, coins)
