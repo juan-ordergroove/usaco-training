e416288b79cb763a3b043f2bc47f25e07f982bec
Juan Gutierrez <juanny.gee@gmail.com>
2020-08-25 14:41:02 -0400

    Add brute force implementation - too slow though

--- Chapter-2/Section-2.3/money/money.py
+++ Chapter-2/Section-2.3/money/money.py
@@ -6,10 +6,24 @@ TASK: money
 
 
 class MoneySystem:
-    def __init__(self, target_value, coins):
-        self.target = target_value
+    def __init__(self, n, v, coins):
+        self.n = n
+        self.v = v
+        self.counter = 0
+
         self.coins = coins
+        self.coins.sort()
 
     def count_combinations(self):
-        if all([c > self.target for c in self.coins]):
-            return 0
+        if not all([c > self.n for c in self.coins]):
+            self._iter()
+        return self.counter
+
+    def _iter(self, i=0, calc=0):
+        for j in range(i, self.v):
+            if calc + self.coins[j] == self.n:
+                self.counter += 1
+                return
+            if calc + self.coins[j] > self.n:
+                return
+            self._iter(j, calc + self.coins[j])
