d795a612d8ce9cb0c92162463c7692049e4206cc
Juan Gutierrez <juanny.gee@gmail.com>
2020-08-31 09:53:36 -0400

    Add support for file based input/output

--- Chapter-2/Section-2.3/money/money.py
+++ Chapter-2/Section-2.3/money/money.py
@@ -21,3 +21,20 @@ class MoneySystem:
                 change_counter[i] += change_counter[i - c]
 
         return change_counter[n]
+
+
+if __name__ == '__main__': # pragma: no cover
+    coins = []
+    v = n = 0
+    with open('money.in') as f:
+        data = f.readline().split(' ')
+        v, n = int(data[0]), int(data[1])
+
+        coin_data = f.readline()
+        while coin_data:
+            coins.extend(map(int, coin_data.split(' ')))
+            coin_data = f.readline()
+
+    money_sys = MoneySystem(coins)
+    with open('money.out', 'w') as f:
+        f.write(f'{money_sys.count_constructions(n)}\n')
